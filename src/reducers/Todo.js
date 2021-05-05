6f429c3f91e54657143993facdeb502be36f85cc
MaoTSUJI <tsuji.map36@gmail.com>
2021-05-05 14:56:08 +0900

    add description @reducers

--- src/reducers/Todo.js
+++ src/reducers/Todo.js
@@ -1,3 +1,4 @@
+// 初期値に保持するオブジェクトとして定義する。todoListの型は配列
 const initialState = {
   todoList: []
 }
@@ -8,7 +9,7 @@ export const todoReducer = (state = initialState, action) => {
       // 新しく追加するTODO
       const todo = action.payload.todo;
       // stateを複製して追加
-      const newState = Object.assign({}, state);
+      const newState = Object.assign({}, state);  // 複製（ディープコピー）
       newState.todoList.push(todo);
       return newState;   // 新しいstateを返す
     default:
